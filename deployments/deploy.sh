3ff51c680a3547d6320873264c8bc780ee995fd4
ris2107 <rishabh210798@gmail.com>
2024-06-22 23:20:47 +0530

    deployment script add

---
+++ deployments/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Stop and remove existing container
+docker stop api-gateway
+docker rm api-gateway
+
+# Remove existing image
+docker rmi node-gateway-app-img
+
+# Build the new image
+docker build -t node-gateway-app-img .
+
+# Wait for 5 seconds (adjust as needed)
+sleep 10
+echo "wait over"
+
+# Run the container with the new image
+docker run -d --name api-gateway --network api-a-network -p 8000:8000 \
+    -e NODE_ENV=development \
+    -e MONGODB_URI="mongodb+srv://db1:2nknGTM21M9BYEwL@api-a-cluster.jhheehg.mongodb.net/?retryWrites=true&w=majority&appName=API-A-CLUSTER" \
+    -v "$(pwd)":/usr/src/app \
+    node-gateway-app-img
