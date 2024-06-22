aa144786170a1840d0857e7dc64f097944a54ac6
ris2107 <rishabh210798@gmail.com>
2024-06-23 03:54:49 +0530

    routes fixed

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,31 @@ router.all("/:destinationAppRoot/*", async (req, res) => {
   const routeDetails = await global.routesCache.getRouteByPath(
     RouteUtils.appendSlashAtStart(destinationAppRoot)
   );
+
   if (routeDetails) {
     const path = req.params[0];
-    axios
-      .get(`${routeDetails.baseUrl}/${path}`)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((error) => {
-        console.error("Error forwarding request:", error);
-        res.status(500).send("Error forwarding request");
+
+    const headers = {};
+    if (routeDetails.headers) {
+      Object.assign(headers, routeDetails.headers);
+    }
+    try {
+      const response = await axios({
+        method: routeDetails.method,
+        url: `${routeDetails.baseUrl}/${path}`,
+        headers: headers,
+        params: routeDetails.queryParams,
+        data: routeDetails.bodyParams,
       });
+
+      res.status(response.status).json(response.data);
+    } catch (error) {
+      console.error("Error forwarding request:", error);
+      res.status(500).send("Error forwarding request");
+    }
   } else {
-    console.error("Route not found for : ", destinationAppRoot);
+    console.error("Route not found for App Root: ", destinationAppRoot);
+    res.status(404).send(`Route not found for App Root ${destinationAppRoot}`);
   }
 });
 
